2135579cdcf3733a1af9b07010a119b595507cd8
Treelar <ninjawarrior1337@users.noreply.github.com>
2020-08-01 18:26:29 -0700

    Update error.vue

--- layouts/error.vue
+++ layouts/error.vue
@@ -4,7 +4,7 @@
           {{error.statusCode}}
             Not Found
             <p>If you find this, congrats! You get an exclusive download of Reverse Game 2</p>
-            <button class="px-4 py-2 bg-blue-500 rounded-lg text-4xl border-b-4 border-blue-700" @click="openRick" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Download</button>
+            <button class="px-4 py-2 bg-blue-500 rounded-lg text-4xl border-b-4 border-blue-700" @click="openRick">Download</button>
       </div>
       <div class="text-6xl text-center" v-else>
           {{error.statusCode}}
