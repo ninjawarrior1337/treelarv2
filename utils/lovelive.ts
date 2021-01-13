4c68862573a80e5a14e1f4b0c025913c6d58a0ae
Treelar <t.rothled@gmail.com>
2021-01-13 11:41:02 -0800

    make love live use json stored at github

--- utils/lovelive.ts
+++ utils/lovelive.ts
@@ -1,10 +1,19 @@
-import { ref } from "@vue/composition-api"
-import {idols} from "../assets/lovelivedata.json"
-import {Idol} from "../types/json-shim"
+import { onMounted, ref } from "@vue/composition-api"
+import { Idol } from "../types/json-shim"
 
 export class LoveLiveUtils {
-    static TODAY = new Date(Date.now())
-    private static getTZDayMonth(tz: string): [string, string] {
+    private TODAY = new Date(Date.now())
+    private idols = [] as Idol[]
+    private IdolDataUrl = "https://gist.githubusercontent.com/ninjawarrior1337/2a51ec53e679550a1d254a465ee79c11/raw"
+    public async setup() {
+        if (this.idols.length > 0) {
+            return
+        }
+        let r = await fetch(this.IdolDataUrl)
+        let j = await r.json()
+        this.idols = j.idols
+    }
+    private getTZDayMonth(tz: string): [string, string] {
         var month = this.TODAY.toLocaleDateString("en-US", {
             timeZone: tz,
             month: "numeric"
@@ -17,7 +26,7 @@ export class LoveLiveUtils {
         return [month, day]
     }
 
-    private static checkBirthday(i: Idol, tz: string): boolean {
+    private checkBirthday(i: Idol, tz: string): boolean {
         const [iMonth, iDay] = i.birthday.split("/")
         const [tMonth, tDay] = this.getTZDayMonth(tz)
         if(tMonth == iMonth && tDay == iDay) {
@@ -26,16 +35,15 @@ export class LoveLiveUtils {
         return false;
     }
 
-    static getBirthdayIdol(): Idol | null {
-        var today = new Date(Date.now())
+    public getBirthdayIdol(): Idol | null {
         //Check JP birthday first
-        for(var i of idols) {
+        for(var i of this.idols) {
             if(this.checkBirthday(i, "Asia/Tokyo")) {
                 return i
             }
         }
         //Check US birthdays if no JP birthdays exist
-        for(var i of idols) {
+        for(var i of this.idols) {
             if(this.checkBirthday(i, "America/Los_Angeles")) {
                 return i
             }
@@ -44,8 +52,14 @@ export class LoveLiveUtils {
     }
 }
 
+const LLUtils = new LoveLiveUtils()
+
 export function useLoveLive() {
-    let birthdayIdol = ref(LoveLiveUtils.getBirthdayIdol())
+    let birthdayIdol = ref()
+    onMounted(async () => {
+        await LLUtils.setup()
+        birthdayIdol.value = LLUtils.getBirthdayIdol()
+    })
     return {
         birthdayIdol
     }
